9e5458c33b8001972353c4033974a30674844e55
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-10-03 04:43:54 +0200

    [span] Explicitly import symbols from tcb namespace

--- mc/span.hpp
+++ mc/span.hpp
@@ -1,3 +1,11 @@
 #pragma once
 
 #include "tcb/span.hpp"
+
+namespace mc {
+using tcb::as_bytes;
+using tcb::as_writable_bytes;
+using tcb::dynamic_extent;
+using tcb::make_span;
+using tcb::span;
+}
