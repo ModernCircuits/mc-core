bb8591147324dcba124d832a8c3653c7138621cd
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-10-06 22:43:16 +0200

    Add more warning flags

--- cmake/mcCompilerWarnings.cmake
+++ cmake/mcCompilerWarnings.cmake
@@ -1,33 +1,54 @@
 add_library(mc_compiler_warnings INTERFACE)
 add_library(mc::compiler_warnings ALIAS mc_compiler_warnings)
 
-if (MSVC)
+if((CMAKE_CXX_COMPILER_ID STREQUAL "MSVC") OR (CMAKE_CXX_COMPILER_FRONTEND_VARIANT STREQUAL "MSVC"))
+    target_compile_options(mc_compiler_warnings INTERFACE /W3)
     if (MC_BUILD_WERROR)
         target_compile_options(mc_compiler_warnings INTERFACE /WX)
     endif (MC_BUILD_WERROR)
-    target_compile_options(mc_compiler_warnings INTERFACE /W3)
 else ()
+    target_compile_options(mc_compiler_warnings
+        INTERFACE
+            -Wall
+            -Wcast-align
+            -Wextra
+            -Wnarrowing
+            -Woverloaded-virtual
+            -Wpedantic
+            -Wreorder
+            -Wshadow
+            -Wsign-compare
+            -Wsign-conversion
+            -Wstrict-aliasing
+            -Wswitch-enum
+            -Wuninitialized
+            -Wunreachable-code
+            -Wunused-parameter
+            -Wzero-as-null-pointer-constant
+            $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>:
+                -Wbool-conversion
+                -Wconditional-uninitialized
+                -Wconstant-conversion
+                -Wconversion
+                -Wextra-semi
+                -Winconsistent-missing-destructor-override
+                -Wint-conversion
+                -Wnullable-to-nonnull-conversion
+                -Wshadow-all
+                -Wshift-sign-overflow
+                -Wshorten-64-to-32
+                -Wunused-private-field
+            >
+            $<$<CXX_COMPILER_ID:AppleClang>:
+                -Wno-poison-system-directories
+            >
+            $<$<CXX_COMPILER_ID:GNU>:
+                -Wlogical-op
+                -Wno-maybe-uninitialized
+                -Wredundant-decls
+            >
+    )
     if (MC_BUILD_WERROR)
         target_compile_options(mc_compiler_warnings INTERFACE -Werror)
     endif (MC_BUILD_WERROR)
-    target_compile_options(mc_compiler_warnings
-            INTERFACE
-                    -Wall
-                    -Wextra
-                    -Wpedantic
-                    -Wcast-align
-                    -Wshadow
-                    -Wunused-parameter
-                    -Wnarrowing
-                    $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>:
-                        #-Weverything
-                    >
-                    $<$<CXX_COMPILER_ID:AppleClang>:
-                        -Wno-poison-system-directories
-                    >
-                    $<$<CXX_COMPILER_ID:GNU>:
-                        #-Wlogical-op
-                    >
-            )
-
-endif (MSVC)
+endif ()
