3fb1ddbe30030f3e5852a9d8f47ebd0e753b2b6b
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-10-11 19:59:22 +0200

    [iterator] Add tests for data/empty & size

---
+++ tests/stdx/iterator.cpp
@@ -0,0 +1,26 @@
+#include "mc/iterator.hpp"
+
+#include "mc/initializer_list.hpp"
+#include "mc/utility.hpp"
+
+#include <catch2/catch.hpp>
+
+TEMPLATE_TEST_CASE(
+    "stl/iterator: data", "[stl][iterator]", char, short, int, float, double)
+{
+    using T = TestType;
+    T cArray[16] {};
+    REQUIRE(mc::data(cArray) == &cArray[0]);
+    REQUIRE(mc::size(cArray) == 16U);
+    REQUIRE_FALSE(mc::empty(cArray));
+
+    auto stdArray = std::array<T, 8> {};
+    REQUIRE(mc::size(stdArray) == stdArray.size());
+    REQUIRE(mc::data(stdArray) == stdArray.data());
+    REQUIRE(mc::data(mc::as_const(stdArray)) == mc::as_const(stdArray).data());
+    REQUIRE_FALSE(mc::empty(stdArray));
+
+    auto iList = { T(), T() };
+    REQUIRE(mc::data(iList) != nullptr);
+    REQUIRE_FALSE(mc::empty(iList));
+}
