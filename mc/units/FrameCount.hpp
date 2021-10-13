9359e25f05c55e2862317fddb5039814afa44806
Tobias Hienzsch <post@tobias-hienzsch.de>
2021-10-13 23:33:55 +0200

    [units] Add FrameCount

--- mc/units/FrameCount.hpp
+++ mc/units/FrameCount.hpp
@@ -1,12 +1,41 @@
 #pragma once
 
+#include "mc/config.hpp"
 #include "mc/cstdint.hpp"
 
 namespace mc {
 
 struct FrameCount {
+    constexpr FrameCount() noexcept = default;
+    constexpr FrameCount(std::uint64_t initialCount) noexcept;
+
+    MC_NODISCARD constexpr auto count() const noexcept -> std::uint64_t;
+    MC_NODISCARD constexpr operator std::uint64_t() const noexcept;
+
 private:
     std::uint64_t count_ { 0 };
 };
+
+constexpr auto operator+=(FrameCount& fc, std::uint64_t countToAdd) noexcept -> FrameCount&;
+
+template <typename T>
+constexpr auto operator+=(FrameCount& fc, T countToAdd) noexcept -> FrameCount&;
+
+// impl
+constexpr FrameCount::FrameCount(std::uint64_t initialCount) noexcept : count_ { initialCount } { }
+constexpr auto FrameCount::count() const noexcept -> std::uint64_t { return count_; }
+constexpr FrameCount::operator std::uint64_t() const noexcept { return count_; }
+
+constexpr auto operator+=(FrameCount& fc, std::uint64_t countToAdd) noexcept -> FrameCount&
+{
+    fc = FrameCount { fc.count() + countToAdd };
+    return fc;
+}
+
+template <typename T>
+constexpr auto operator+=(FrameCount& fc, T countToAdd) noexcept -> FrameCount&
+{
+    return fc += static_cast<std::uint64_t>(countToAdd);
+}
 
 } // namespace mc
---
+++ tests/units/FrameCount.cpp
@@ -0,0 +1,25 @@
+#include "mc/units/FrameCount.hpp"
+
+#include <catch2/catch.hpp>
+
+TEST_CASE("stl/units: FrameCount", "[stl][units]")
+{
+    // .count()
+    REQUIRE(mc::FrameCount().count() == 0ULL);
+    REQUIRE(mc::FrameCount(0).count() == 0ULL);
+    REQUIRE(mc::FrameCount(1).count() == 1ULL);
+
+    // implicit conversion
+    REQUIRE(mc::FrameCount(0) == 0ULL);
+
+    // operations
+    auto fc = mc::FrameCount { 0 };
+    fc += 1ULL;
+    REQUIRE(fc == 1ULL);
+    fc += 2;
+    REQUIRE(fc == 3ULL);
+    fc += 4.0f;
+    REQUIRE(fc == 7ULL);
+    fc += 8.0;
+    REQUIRE(fc == 15ULL);
+}
