e35af735086ad0ae7054e682e477fc3d8bb609bb
Tobias Hienzsch <tobias@modern-circuits.com>
2021-12-02 19:29:34 +0100

    Fix windows 17 build

--- .gitlab-ci/run-windows-cpp17.bat
+++ .gitlab-ci/run-windows-cpp17.bat
@@ -2,7 +2,7 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 @echo on
 
-cd ..
+cd "%~dp0\.."
 cmake -S . -B cmake-build-release -G "Ninja Multi-Config" -D CMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=17 -DMC_BUILD_WERROR=ON || goto :error
 cmake --build cmake-build-release --config Release || goto :error
 
