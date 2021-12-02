ebfb3b2b147c09fb862ae359d954bf5c23a634c4
Tobias Hienzsch <tobias@modern-circuits.com>
2021-12-02 19:19:38 +0100

    Format code

--- mc/units/FrameCount.hpp
+++ mc/units/FrameCount.hpp
@@ -17,25 +17,17 @@ private:
     std::uint64_t count_ { 0 };
 };
 
-constexpr auto operator+=(FrameCount& fc, std::uint64_t countToAdd) noexcept
-    -> FrameCount&;
+constexpr auto operator+=(FrameCount& fc, std::uint64_t countToAdd) noexcept -> FrameCount&;
 
 template <typename T>
 constexpr auto operator+=(FrameCount& fc, T countToAdd) noexcept -> FrameCount&;
 
 // impl
-constexpr FrameCount::FrameCount(std::uint64_t initialCount) noexcept
-    : count_ { initialCount }
-{
-}
-constexpr auto FrameCount::count() const noexcept -> std::uint64_t
-{
-    return count_;
-}
+constexpr FrameCount::FrameCount(std::uint64_t initialCount) noexcept : count_ { initialCount } { }
+constexpr auto FrameCount::count() const noexcept -> std::uint64_t { return count_; }
 constexpr FrameCount::operator std::uint64_t() const noexcept { return count_; }
 
-constexpr auto operator+=(FrameCount& fc, std::uint64_t countToAdd) noexcept
-    -> FrameCount&
+constexpr auto operator+=(FrameCount& fc, std::uint64_t countToAdd) noexcept -> FrameCount&
 {
     fc = FrameCount { fc.count() + countToAdd };
     return fc;
