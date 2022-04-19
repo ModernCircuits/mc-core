92b69d14c7e7c2ec4bc30a14ee44a1c7230b0e09
Tobias Hienzsch <tobias@modern-circuits.com>
2022-04-19 08:34:16 +0200

    Test batch script on windows pipeline

--- .gitlab-ci/run-windows-cpp17.bat
+++ .gitlab-ci/run-windows-cpp17.bat
@@ -1,5 +1,5 @@
 @echo on
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 @echo on
 
 cd "%~dp0\.."
