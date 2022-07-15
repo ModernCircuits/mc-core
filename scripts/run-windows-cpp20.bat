c2cb178de8dab9a8fc6af06f108c7fe082c6a165
Tobias Hienzsch <tobias@modern-circuits.com>
2022-07-15 22:48:11 +0200

    Fix linux & windows builds

--- scripts/run-windows-cpp20.bat
+++ scripts/run-windows-cpp20.bat
@@ -3,8 +3,8 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliar
 @echo on
 
 cd "%~dp0\.."
-conan install -if cmake-build-release --build=missing -pr:b=default -pr:h=default -s compiler.cppstd=20 -s build_type=Release || goto :error
-conan build -bf cmake-build-release || goto :error
+conan install -if cmake-build-release --build=missing -pr:b=default -pr:h=default -s compiler.cppstd=20 -s build_type=Release . || goto :error
+conan build -bf cmake-build-release . || goto :error
 
 goto :EOF
 :error
