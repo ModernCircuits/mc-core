79696ad8cb43e3638b81a44097b4940ab706608a
Tobias Hienzsch <tobias@modern-circuits.com>
2022-07-29 19:43:52 +0200

    Link CMake Threads library.
    Needed to link on FreeBSD

--- src/mc-core-config.cmake
+++ src/mc-core-config.cmake
@@ -6,6 +6,7 @@ find_dependency(Boost 1.78 REQUIRED COMPONENTS container filesystem json)
 find_dependency(fmt REQUIRED)
 find_dependency(concurrentqueue REQUIRED)
 find_dependency(readerwriterqueue REQUIRED)
+find_dependency(Threads REQUIRED)
 find_dependency(xsimd REQUIRED)
 
 include("${CMAKE_CURRENT_LIST_DIR}/mc-core-targets.cmake")
