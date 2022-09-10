05facac9e98d1177e1c15d091e3b39b7879c191a
Tobias Hienzsch <tobias@modern-circuits.com>
2022-09-10 18:46:28 +0200

    [queue] Ignore warnings on MSVC generate by concurrentqueue

--- src/mc/core/_queue/lock_free_queue.hpp
+++ src/mc/core/_queue/lock_free_queue.hpp
@@ -1,9 +1,20 @@
 // SPDX-License-Identifier: BSL-1.0
 #pragma once
 
+#include <mc/core/config.hpp>
+
+#if defined(MC_COMPILER_MSVC)
+#pragma warning(push)
+#pragma warning(disable: 4702)  // unreachable code
+#endif
+
 #include <moodycamel/concurrentqueue.h>
 #include <readerwriterqueue/readerwritercircularbuffer.h>
 #include <readerwriterqueue/readerwriterqueue.h>
+
+#if defined(MC_COMPILER_MSVC)
+#pragma warning(pop)
+#endif
 
 namespace mc {
 template<typename T>
