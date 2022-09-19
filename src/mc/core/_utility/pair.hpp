9b25bdcace054823a49ea51ce6b1d9b7a1e1b500
Tobias Hienzsch <tobias@modern-circuits.com>
2022-09-19 21:45:36 +0200

    [utility] Add pair alias

---
+++ src/mc/core/_utility/pair.hpp
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: BSL-1.0
+#pragma once
+
+#include <mc/core/config.hpp>
+
+#include <utility>
+
+namespace mc {
+
+template<typename T, typename U>
+using Pair = std::pair<T, U>;
+
+}  // namespace mc
