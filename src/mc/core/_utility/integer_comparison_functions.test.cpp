eed608c5789342af24404e0801fca9066cf697b4
Tobias Hienzsch <tobias@modern-circuits.com>
2022-09-30 11:23:07 +0200

    [utility] More tests for integer_comparison_functions

--- src/mc/core/_utility/integer_comparison_functions.test.cpp
+++ src/mc/core/_utility/integer_comparison_functions.test.cpp
@@ -27,6 +27,16 @@ TEST_CASE("utility.hpp: integer_comparison_functions", "[utility]")  // NOLINT
     REQUIRE_FALSE(cmp_greater(-1, 1U));
     REQUIRE_FALSE(cmp_greater_equal(-1, 1U));
 
+    REQUIRE(cmp_less(-99, 1U));
+    REQUIRE(cmp_less_equal(-99, 1U));
+    REQUIRE_FALSE(cmp_greater(-99, 1U));
+    REQUIRE_FALSE(cmp_greater_equal(-99, 1U));
+
+    REQUIRE(cmp_greater(1U, -1));
+    REQUIRE(cmp_greater_equal(1U, -1));
+    REQUIRE_FALSE(cmp_less(1U, -1));
+    REQUIRE_FALSE(cmp_less_equal(1U, -1));
+
     // REQUIRE(-1 == 0xFFFFFFFFU);  //< warning: sign-unsign comparison
     REQUIRE(cmp_not_equal(-1, 0xFFFFFFFFU));
 }
