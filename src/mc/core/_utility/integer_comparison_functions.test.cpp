7b2bdb94c5e6a6159d419bea0baddf8e187d90ff
Tobias Hienzsch <tobias@modern-circuits.com>
2022-09-30 11:05:02 +0200

    [utility] Add tests to integer_comparison_functions

---
+++ src/mc/core/_utility/integer_comparison_functions.test.cpp
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: BSL-1.0
+
+#include <mc/core/utility.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+
+using namespace mc;
+
+TEST_CASE("utility.hpp: integer_comparison_functions", "[utility]")  // NOLINT
+{
+    using namespace mc;
+
+    STATIC_REQUIRE(sizeof(int) == 4);  // precondition
+
+    // Quite surprisingly
+    // REQUIRE(-1 > 1U);  //< warning: sign-unsign comparison
+    // because after implicit conversion of -1 to the RHS type (`unsigned int`)
+    // the expression is equivalent to:
+
+    REQUIRE(0xFFFFFFFFU > 1U);
+    REQUIRE(0xFFFFFFFFU == static_cast<unsigned>(-1));
+
+    // In contrast, the cmp_* family compares integers as most expected -
+    // negative signed integers always compare less than unsigned integers:
+    REQUIRE(cmp_less(-1, 1U));
+    REQUIRE(cmp_less_equal(-1, 1U));
+    REQUIRE_FALSE(cmp_greater(-1, 1U));
+    REQUIRE_FALSE(cmp_greater_equal(-1, 1U));
+
+    // REQUIRE(-1 == 0xFFFFFFFFU);  //< warning: sign-unsign comparison
+    REQUIRE(cmp_not_equal(-1, 0xFFFFFFFFU));
+}
